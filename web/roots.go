a9ddac616f1b1edef372d984a0f7e0ec390d079e
Zakaria Boualaid <boulaidzac@gmail.com>
2026-07-04 13:16:19 +0200

    support x-nexqloud-entity

--- web/roots.go
+++ web/roots.go
@@ -2,42 +2,8 @@
 
 package main
 
-import (
-	_ "embed"
-	"fmt"
-
-	"github.com/google/go-sev-guest/kds"
-
-	"nexqloud-sealed/pkg/verify"
-)
-
-//go:embed certs/ask_ark_milan.pem
-var askArkMilan []byte
-
-//go:embed certs/ask_ark_genoa.pem
-var askArkGenoa []byte
-
-//go:embed certs/ask_ark_turin_vcek.pem
-var askArkTurin []byte
+import "nexqloud-sealed/pkg/verify"
 
 func loadHardwareRootsCatalog() (map[string]verify.HardwareRoots, error) {
-	bundles := map[string][]byte{
-		"Milan": askArkMilan,
-		"Genoa": askArkGenoa,
-		"Turin": askArkTurin,
-	}
-
-	out := make(map[string]verify.HardwareRoots, len(bundles))
-	for product, pem := range bundles {
-		ask, ark, err := kds.ParseProductCertChain(pem)
-		if err != nil {
-			return nil, fmt.Errorf("parse %s ASK/ARK bundle: %w", product, err)
-		}
-		out[product] = verify.HardwareRoots{
-			ProductLine: product,
-			ASK:         ask,
-			ARK:         ark,
-		}
-	}
-	return out, nil
+	return verify.LoadHardwareRootsCatalog()
 }
